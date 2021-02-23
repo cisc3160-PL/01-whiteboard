166ee8597879f44f3e2e25453ed4267208345404
Ricky Chon <rickychon99@gmail.com>
2021-02-23 02:34:13 -0500

    Add event listeners for mouse movement and clicks
    Throttling mousemove event to not occur more than 10 times a second

--- scripts/whiteboard.js
+++ scripts/whiteboard.js
@@ -4,4 +4,41 @@
 {
     let canvas = document.getElementById('whiteboard');
     let ctx = canvas.getContext('2d');
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mouseout', onMouseUp, false);
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+    function onMouseDown(event)
+    {
+        console.log('mouse down');
+    }
+
+    function onMouseUp(event)
+    {
+        console.log('mouse up');
+    }
+
+    function onMouseMove(event)
+    {
+        console.log('mouse move');
+    }
+
+    // Throttle number of events executed per second
+    function throttle(cb, delay)
+    {
+        let prevCall = new Date().getTime();
+
+        return function()
+        {
+            let currTime = new Date().getTime();
+            
+            if(currTime - prevCall >= delay)
+            {
+                prevCall = currTime;
+                cb.apply(null, arguments);
+            }
+        };
+    }
 })();
